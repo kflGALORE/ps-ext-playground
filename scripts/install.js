2d30c8e84fbcdb824c6894ea98ecd1f619b5b809
kflGALORE <kflgalore@gmail.com>
2020-05-20 21:11:06 +0200

    automation:install

--- scripts/install.js
+++ scripts/install.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+
+console.log(process.platform);
 console.log(process.env);
+
+console.log(installDir());
+console.log(fs.statSync(installDir()));
+
+function installDir() {
+    if (process.platform.toLowerCase().startsWith('win')) {
+        return process.env.APPDATA + '\\Adobe\\CEP\\extensions';
+    } else {
+        return process.env.HOME + '/Library/Application Support/Adobe/CEP/extensions';
+    }
+}
